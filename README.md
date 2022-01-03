1b9bf7e867fe69286144e8692dc8574b6093e0aa
muki <filipdobranic@gmail.com>
2022-01-03 12:06:08 +0100

    add documentation to sync media

---
+++ README.md
@@ -0,0 +1,9 @@
+# How to migrate from PVC to S3 storage
+
+[x] Update secrets.
+[x] Deploy.
+[x] After deployment is finished you won't see any user-uploaded images.
+[x] Sync old media files by editing lines 6 and 7 and then running `./syncmedia.sh`.
+[ ] Remove the PVC binding from the deployment.
+[ ] Tripple check everything is working.
+[ ] Delete the PVC.
---
+++ syncmedia.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#################################
+# set these up before you begin #
+#################################
+FROM_POD_NAME="pravnamreza-staging-65fc5b6b45-4shxm"
+NAMESPACE="pravnamreza-staging"
+
+echo "WARNING! If you did not update the code, don't bother running this. Update the code on production first!"
+read -p "Are you sure you want to upload files to S3? [y/N]" -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    echo "Exiting ..."
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+fi
+
+echo
+
+#################################
+# user confirmed, let's do this #
+#################################
+echo "Downloading FROM -> localhost"
+# copy FROM to localhost
+kubectl cp $NAMESPACE/$FROM_POD_NAME:/pvc/media ./backup/pvc/media/
+echo "Done downloading FROM -> localhost"
+
+echo "I will now attempt to upload files to S3."
+aws s3 sync ./backup/pvc/media s3://djnd/pravnamreza --acl public-read
+
+echo "All done. Now go and check if it worked correctly."
