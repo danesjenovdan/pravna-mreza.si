ec53b15dda53485067040a7a2280ba01a7e2ea9d
muki <filipdobranic@gmail.com>
2022-01-25 08:14:51 +0100

    minor syncmedia.sh refactor

--- syncmedia.sh
+++ syncmedia.sh
@@ -3,13 +3,14 @@
 #################################
 # set these up before you begin #
 #################################
-FROM_POD_NAME="pravnamreza-staging-65fc5b6b45-4shxm"
-NAMESPACE="pravnamreza-staging"
+FROM_POD_NAME="pravnamreza-986bc9cf7-rx5bx"
+NAMESPACE="pravnamreza"
 
 echo "WARNING! If you did not update the code, don't bother running this. Update the code on production first!"
-read -p "Are you sure you want to upload files to S3? [y/N]" -n 1 -r
+read -p "Are you sure you want to migrate storage to S3? [y/N]" -n 1 -r
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
+    echo
     echo "Exiting ..."
     [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
 fi
@@ -25,6 +26,13 @@ kubectl cp $NAMESPACE/$FROM_POD_NAME:/pvc/media ./backup/pvc/media/
 echo "Done downloading FROM -> localhost"
 
 echo "I will now attempt to upload files to S3."
+read -p "Are you sure you want to upload files to S3? [y/N]" -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    echo
+    echo "Exiting ..."
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+fi
 aws s3 sync ./backup/pvc/media s3://djnd/pravnamreza --acl public-read
 
 echo "All done. Now go and check if it worked correctly."
