5c06c50e1bffa95d485142f776e7b4086fbc2f53
patricijab <patricija.brecko@gmail.com>
2022-02-18 08:26:05 +0100

    make django serve static

--- pravna-mreza.si/pravnamreza/urls.py
+++ pravna-mreza.si/pravnamreza/urls.py
@@ -3,6 +3,8 @@ from django.urls import include, path
 from django.contrib import admin
 from django.views.generic import RedirectView
 from django.conf.urls import url
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
@@ -19,13 +21,10 @@ urlpatterns = [
     path('documents/', include(wagtaildocs_urls)),
 
     # path('search/', search_views.search, name='search'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 if settings.DEBUG:
-    from django.conf.urls.static import static
-    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
